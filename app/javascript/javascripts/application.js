0d97edf9a5fd251cc4727ccc9f4e354d98843ab6
yoshida daiki <yoshida.daiki.51@gmail.com>
2023-06-08 21:31:34 +0900

    スタイルシートの読み込み変更

--- app/javascript/javascripts/application.js
+++
@@ -1,6 +0,0 @@
-// Entry point for the build script in your package.json
-// The entry point is the file that Webpack will look for to start building your assets. By default, Webpack will look for a file named application.js in the app/javascript/packs directory. This is the file that Rails generates for you when you run rails webpacker:install.
-// 
-// If you want to change the name of this file, you can do so by updating the entry key in the webpacker configuration file. For example, if you wanted to change the entry point to app/javascript/application.js, you would update the entry key in config/webpacker.yml to look like this:
-
-import '../stylesheets/application.scss'
