e6d0d6516378cc7bee2c7281a7b5b4ebe1fe2b74
yoshida daiki <yoshida.daiki.51@gmail.com>
2023-07-06 00:02:50 +0900

    ユーザー登録とログイン画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: t('.success')
+      redirect_to root_path, success: t('.success')
     else
       flash.now[:warning] = t('.failure')
       render :new
