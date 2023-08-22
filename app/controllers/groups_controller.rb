ce7148e41a146ddac84dedf1858fa5ceabc39475
yoshida daiki <yoshida.daiki.51@gmail.com>
2023-08-23 02:56:56 +0900

    グループコントローラーにトランザクション追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,10 +7,12 @@ class GroupsController < ApplicationController
   def create
     group = Group.new(name: params[:name])
     if group.save
-      group.users << current_user
-      group.users << User.where(id: params[:group][:user_ids])
-      @matching = Matching.create(name: group.name, group_id: group.id, public_flag: false)
-      @matching_profile = MatchingProfile.create(matching_id: @matching.id)
+      ActiveRecord::Base.transaction do
+        group.users << current_user
+        group.users << User.where(id: params[:group][:user_ids])
+        matching = Matching.create(name: group.name, group_id: group.id, public_flag: false)
+        @matching_profile = MatchingProfile.create(matching_id: matching.id)
+      end
       redirect_to matching_profile_path(@matching_profile), success: t('.success')
     else
       @group = Group.new(name: params[:name])
@@ -21,10 +23,12 @@ class GroupsController < ApplicationController
   end
 
   def leave
-    group = Matching.find(params[:id]).group
-    current_user.delete_approved_chat_request_for_matching(group) # マッチングに対するチャットリクエストを削除する
-    group.users.delete(current_user)
-    group.destroy_if_empty # グループが空になったら削除する
+    ActiveRecord::Base.transaction do
+      group = Matching.find(params[:id]).group
+      current_user.delete_approved_chat_request_for_matching(group) # マッチングに対するチャットリクエストを削除する
+      group.users.delete(current_user)
+      group.destroy_if_empty # グループが空になったら削除する
+    end
     redirect_to matchings_path, success: t('.success')
   end
 
