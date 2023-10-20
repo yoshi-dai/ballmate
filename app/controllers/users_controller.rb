49f789bbe84b3b7cf12f4045f8bea50a2b43141b
yoshida daiki <yoshida.daiki.51@gmail.com>
2023-10-20 22:09:54 +0900

    ユーザー一覧の各コントローラーのレンダー先を削除して、各々のビューページに遷移するようにするところまで。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,26 +42,22 @@ class UsersController < ApplicationController
 
   def matched_users
     @users = @q.result(distinct: true).where(id: current_user.approved_chat_requests.flat_map { |cr| [cr.sender_id, cr.receiver_id] }.uniq).where.not(id: current_user.id).page(params[:page])
-    render 'users/index'
   end
 
   def requested_users
     @chat_requests = current_user.sent_chat_requests.includes(receiver: :user_profile).where(status: 'pending').where.not(receiver: nil)
     @users = @q.result(distinct: true).where(id: @chat_requests.map(&:receiver_id)).where.not(id: current_user.id).page(params[:page])
-    render 'users/index'
   end
 
   def approval_pending_users
     @chat_requests = current_user.received_chat_requests.includes(sender: :user_profile).where(status: 'pending')
     @users = @q.result(distinct: true).where(id: @chat_requests.map(&:sender_id)).where.not(id: current_user.id).page(params[:page])
-    render 'users/index'
   end
 
   def matching_having_users
     @matching = Matching.find(params[:matching_id])
     @users = @q.result(distinct: true).where(id: @matching.group.users.map(&:id)).where.not(id: current_user.id).page(params[:page])
     @chat_request = ChatRequest.new # rendersした先でチャットリクエストを送るために@chat_requestを定義している
-    render 'users/index' # renderした先でチャット申請の処理を行うといちいちmatching_having_users画面から離れてしまうため、チャット申請を行う処理を非同期で行えるようにした方がいい(ただしindexとmatching_having_usersでちがうから少し考える必要あり)
   end
 
   private
