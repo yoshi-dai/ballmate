02603316b31dcfc2f12589977a0805a91986ea1f
yoshida daiki <yoshida.daiki.51@gmail.com>
2023-10-26 18:30:43 +0900

    matched_users、requested_users、approval_pending_users、matching_having_usersのアクションをメソッドを切り分けて整理するところまで。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: %i[new create]
+  before_action :set_user, only: %i[show destroy]
   before_action :set_q, only: %i[index matched_users requested_users approval_pending_users matching_having_users]
-  before_action :excluded_user_ids, only: %i[index]
 
   def index
     @users = filtered_index_users.page(params[:page])
@@ -9,7 +9,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_profile = @user.user_profile
   end
 
@@ -29,28 +28,27 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy!
     redirect_to :root, success: t('.success')
   end
 
   def matched_users
-    @users = @q.result(distinct: true).where(id: current_user.approved_chat_requests.flat_map { |cr| [cr.sender_id, cr.receiver_id] }.uniq).where.not(id: current_user.id).page(params[:page])
+    @users = filtered_matched_users.page(params[:page])
   end
 
   def requested_users
-    @chat_requests = current_user.sent_chat_requests.includes(receiver: :user_profile).where(status: 'pending').where.not(receiver: nil)
-    @users = @q.result(distinct: true).where(id: @chat_requests.map(&:receiver_id)).where.not(id: current_user.id).page(params[:page])
+    @chat_requests = current_user.sent_chat_requests.where(status: 'pending')
+    @users = filterd_requested_users.page(params[:page])
   end
 
   def approval_pending_users
-    @chat_requests = current_user.received_chat_requests.includes(sender: :user_profile).where(status: 'pending')
-    @users = @q.result(distinct: true).where(id: @chat_requests.map(&:sender_id)).where.not(id: current_user.id).page(params[:page])
+    @chat_requests = current_user.received_chat_requests.where(status: 'pending')
+    @users = filterd_approval_pending_users.page(params[:page])
   end
 
   def matching_having_users
     @matching = Matching.find(params[:matching_id])
-    @users = @q.result(distinct: true).where(id: @matching.group.users.map(&:id)).where.not(id: current_user.id).page(params[:page])
+    @users = filtered_matching_having_users.page(params[:page])
     @chat_request = ChatRequest.new # rendersした先でチャットリクエストを送るために@chat_requestを定義している
   end
 
@@ -79,7 +77,24 @@ class UsersController < ApplicationController
 
   def filtered_index_users
     @q.result(distinct: true)
-    .where.not(id: excluded_user_ids)
-    .where.not(user_profiles: { name: nil })
+      .where.not(id: excluded_user_ids)
+      .includes(:user_profile)
+      .where.not(user_profiles: { name: nil })
+  end
+
+  def filtered_matched_users
+    @q.result(distinct: true).where(id: current_user.approved_chat_requests.flat_map { |cr| [cr.sender_id, cr.receiver_id] }.uniq).where.not(id: current_user.id).includes(:user_profile)
+  end
+
+  def filterd_requested_users
+    @q.result(distinct: true).where(id: @chat_requests.map(&:receiver_id)).where.not(id: current_user.id).includes(:user_profile)
+  end
+
+  def filterd_approval_pending_users
+    @q.result(distinct: true).where(id: @chat_requests.map(&:sender_id)).where.not(id: current_user.id).includes(:user_profile)
+  end
+
+  def filtered_matching_having_users
+    @q.result(distinct: true).where(id: @matching.group.users.map(&:id)).where.not(id: current_user.id).includes(:user_profile)
   end
 end
