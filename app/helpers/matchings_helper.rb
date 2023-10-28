e9b7082cb7d135a22a09b83b5b9a41119c6079d7
yoshida daiki <yoshida.daiki.51@gmail.com>
2023-10-29 03:07:02 +0900

    マッチングのdestroyアクションのdestroyアクションと通知削除メソッドをヘルパーで分けるところまで。

--- app/helpers/matchings_helper.rb
+++ app/helpers/matchings_helper.rb
@@ -1,2 +1,10 @@
 module MatchingsHelper
+  def delete_matching!(matching)
+    ActiveRecord::Base.transaction do
+      matching.users.each do |user|
+        user.delete_approved_chat_request_for_user(matching)
+      end
+      matching.destroy!
+    end
+  end
 end
